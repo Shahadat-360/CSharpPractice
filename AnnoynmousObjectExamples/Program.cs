ec294b8a5c6d648f506e4fe0e1e1d653a70b55fd
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-06-14 23:02:00 +0600

    dynamic keyword and annonymous Object

---
+++ AnnoynmousObjectExamples/Program.cs
@@ -0,0 +1,13 @@
+﻿//object o = new { }; // syntax for annonymous object
+var o = new { Name = "MD. Shahadat", Roll = 190621 };
+Console.WriteLine(o.Name);
+Console.WriteLine(o.Roll);
+
+dynamic Get2Number()
+{
+    return new { n1 = 1.5, n2 = 2.5 };
+}
+
+var res = Get2Number();
+Console.WriteLine(res.n1);
+Console.WriteLine(res.n2);
