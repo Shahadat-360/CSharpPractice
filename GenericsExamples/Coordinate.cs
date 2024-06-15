cca55208793dd4eea1da4ddc30bcb1abe1f77b68
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-06-15 23:35:19 +0600

    Generics Examples

---
+++ GenericsExamples/Coordinate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExamples
+{
+    internal class Coordinate<T>
+    where T : IEquatable<T>
+    {
+        public T x { get; set; }
+        public T y { get; set; }
+
+        public Coordinate(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void ThreeDimension<R, S>(R item,S item2)
+        {
+            Console.WriteLine($"{item} {item2}");
+        }
+        public bool isSame(T x,T y) { 
+            return x.Equals(y);
+        }
+    }
+}
---
+++ GenericsExamples/Program.cs
@@ -0,0 +1,8 @@
+﻿using GenericsExamples;
+
+Coordinate<double> coordinate = new Coordinate<double>(5,6);
+//coordinate.x = 50;
+//coordinate.y = 100;
+Console.WriteLine(coordinate.x+coordinate.y);
+coordinate.ThreeDimension(5, "Shahdat");
+Console.WriteLine(coordinate.isSame(5, 5));
