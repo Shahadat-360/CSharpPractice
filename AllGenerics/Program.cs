6ee5f0b8fef7beebbe593174cd7c1874beb77c72
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-06-28 15:54:54 +0600

    All Examples of Generics

---
+++ AllGenerics/Program.cs
@@ -0,0 +1,26 @@
+﻿#region List Examples
+List<int> list = new List<int>();
+list.Add(0);
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+list.Add(5);
+list.Add(6);
+list.Add(7);
+list.Add(8);
+list.Add(9);
+list.Add(10);
+foreach (var x in list) Console.Write(x.ToString() + " ");
+Console.WriteLine();
+#endregion
+
+#region Dictionary
+Dictionary<string,int> Result= new Dictionary<string,int>();
+Result.Add("Shahadat", 100);
+Result.Add("Masud", 150);
+Result.Add("Shourov", 200);
+Result.Add("Mitu", 250);
+foreach(var x in Result)Console.WriteLine(x.Key + ": " + x.Value);  
+#endregion
+
