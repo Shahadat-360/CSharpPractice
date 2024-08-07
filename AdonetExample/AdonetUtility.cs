9704b77fff6d4ad09022063960f6685d444b63b8
MD. Shahadat Hossain Bhuian <1337.shahadat@gmail.com>
2024-08-07 12:48:01 +0600

    AdonetExample

---
+++ AdonetExample/AdonetUtility.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace AdonetExample
+{
+    internal class AdonetUtility : IDisposable
+    {
+        private readonly SqlConnection _connection;
+        public AdonetUtility(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+
+        public void ExecuteCommand(string commandText, Dictionary<string, object> parameters = null)
+        {
+            using SqlCommand command = CreateCommand(commandText, parameters);
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine(affected);
+        }
+
+        public List<Dictionary<string, object>> ExecuteQuery(string commandText, Dictionary<string, object> parameters = null)
+        {
+            using SqlCommand command = CreateCommand(commandText, parameters);
+            using SqlDataReader reader = command.ExecuteReader();
+            List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
+            while (reader.Read())
+            {
+                Dictionary<string, object> row = new Dictionary<string, object>();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row.Add(reader.GetName(i), reader.GetValue(i));
+                }
+                result.Add(row);
+            }
+            return result;
+        }
+
+        private SqlCommand CreateCommand(string sql, Dictionary<string, object> parameters)
+        {
+            SqlCommand command = new SqlCommand(sql, _connection);
+            if (parameters != null)
+            {
+                foreach (var parameter in parameters)
+                {
+                    command.Parameters.Add(new SqlParameter(parameter.Key, parameter.Value));
+                }
+            }
+            if (command.Connection.State != System.Data.ConnectionState.Open)
+            {
+                command.Connection.Open();
+            }
+            return command;
+        }
+    }
+}
---
+++ AdonetExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using AdonetExample;
+
+//var FirstName=Console.ReadLine();
+//var LastName=Console.ReadLine();
+//var DateOfBirth=DateTime.Parse(Console.ReadLine());
+//var IsMarried=bool.Parse(Console.ReadLine());
+
+var connectionString = "Server=SHAHADAT-PC\\SQLEXPRESS;Database=CSharpB17;User Id=CSharpB17;Password=123456;Trust Server Certificate=True";
+
+var InsertCommandText = $"insert into Customers(FirstName,LastName,DateOfBirth,IsMarried) values(@FirstName,@LastName,@DateOfBirth,@IsMarried)";
+var DeleteCommandText = "delete from Customers where id=1006";
+var SelectQuery = "select * from Customers";
+
+//Dictionary<string,object> Parameters = new Dictionary<string,object>();
+//Parameters.Add("FirstName",FirstName);
+//Parameters.Add("LastName", LastName);
+//Parameters.Add("DateOfBirth", DateOfBirth);
+//Parameters.Add("IsMarried",IsMarried);
+
+using AdonetUtility adonetUtility = new AdonetUtility(connectionString);
+adonetUtility.ExecuteCommand(DeleteCommandText);
+
+var result = adonetUtility.ExecuteQuery(SelectQuery);
+
+foreach (var item in result)
+{
+    foreach (var row in item)
+    {
+        Console.Write($"{row.Key}: {row.Value}  ");
+    }
+    Console.WriteLine();
+}
