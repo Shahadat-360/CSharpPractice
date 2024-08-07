7b2c915ce02e9c316c0b52eca95a0cbfeb1ef18e
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-08-07 11:58:01 +0600

    File System Example

---
+++ FileSystemExample/Program.cs
@@ -0,0 +1,21 @@
+﻿//using System.Text;
+
+//string text = Console.ReadLine();
+//byte[] bytes = Encoding.UTF8.GetBytes(text);
+//using FileStream file = File.Create("hello.txt");
+//file.Write(bytes);
+
+//FileInfo file1 = new FileInfo("Hello2.txt");
+//using FileStream stream = file1.Create();
+//stream.Write(bytes);
+
+using System.Text;
+
+byte[] bytes = new byte[2];
+using FileStream fileStream = File.Open("hello.txt", FileMode.Open);
+long byteSize = fileStream.Length;
+for (int i = 0; i < byteSize; i += 2)
+{
+    fileStream.Read(bytes, 0, 2);
+    Console.WriteLine(Encoding.UTF8.GetString(bytes));
+}
