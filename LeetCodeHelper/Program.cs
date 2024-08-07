10d11e2140cda7ddbfa39f96cc8d02c58775e415
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-08-07 11:58:32 +0600

    Leet Code Helper

---
+++ LeetCodeHelper/Program.cs
@@ -0,0 +1,14 @@
+﻿int n; 
+int[][] roads = new int[2][];
+Dictionary<int, int> map = new Dictionary<int, int>();
+for (int i = 0; i < roads.Length; i++)
+{
+    int cell = roads[i][0];
+    map[cell] = map.GetValueOrDefault(cell, 0) + 1;
+}
+for (int i = 0; i < roads.Length; i++)
+{
+    int cell = roads[i][1];
+    map[cell] = map.GetValueOrDefault(cell, 0) + 1;
+}
+var m = map.OrderByDescending
