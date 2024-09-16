9c0c2e714420e9e4cd3f97bcf95c1bf22e3cff3d
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-16 20:27:17 +0600

    Liskov Substitution of SOLID Principles

---
+++ SOLIDPrincipleExample/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrincipleExample
+{
+    public class Board
+    {
+        public int[,] grid;
+        public void SetItem(int x, int y,int value)
+        {
+            grid[x,y] = value;  
+        }
+        public void RemoveItem(int x, int y)
+        {
+            grid[x, y] = 0;
+        }
+    }
+}
---
+++ SOLIDPrincipleExample/Board3D.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrincipleExample
+{
+    public class Board3D
+    {
+        public int[,,] grid3D;
+        public void SetItem(int x, int y, int z,int value)
+        {
+            grid3D[x,y,z] = value;
+        }
+        public void RemoveItem(int x, int y, int z)
+        {
+            grid3D[x, y, z] = 0;
+        }
+    }
+}
