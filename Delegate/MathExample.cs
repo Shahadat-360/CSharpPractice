2deb1e348627e0b4d932df76f8e0624eae8730ca
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-18 20:15:28 +0600

    Delegate

---
+++ Delegate/MathExample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class MathExample
+    {
+       public delegate int Calculate(int a, int b);
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,16 @@
+﻿using static Delegate.MathExample;
+
+//Calculate addition = sum;
+//int sum(int a,int b)=>a + b;
+//int multiply(int a,int b) => a * b;
+
+
+void print(int a,int b,Calculate calculate)
+{
+    Console.WriteLine(calculate(a,b));
+}
+
+print(1,2,(int a,int b)=>a*b);
+
+//Console.WriteLine(addition(5, 6)); 
+//Console.WriteLine(multiply(5, 6));
