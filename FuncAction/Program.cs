2b60cd1b3717c3d1b281e13dfc4533f938f754ad
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-18 20:15:54 +0600

    Func and Action

---
+++ FuncAction/Program.cs
@@ -0,0 +1,15 @@
+﻿//print(1,2,(int a,int b)=>a*b);
+
+//Func 
+//void print(int a,int b,Func<int,int,int> calculate)
+//{
+//    Console.WriteLine(calculate(a,b));
+//}
+
+//Action
+//var x =(int a,int b) => { Console.WriteLine(a*b); };
+print(2, 3, (int a, int b) => { Console.WriteLine(a * b); });
+void print(int a,int b,Action<int,int> action)
+{
+    action(a,b);
+}
