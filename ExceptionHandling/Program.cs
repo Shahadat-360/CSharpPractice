60bce74555c891d58c9e0822c8615926adc6eba5
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-19 01:11:43 +0600

    Exception Handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    Console.Write("Please Enter a Number: ");
+    int n = int.Parse(Console.ReadLine());  
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Please Enter Number Not String");
+    Console.WriteLine(ex.ToString());
+}
+finally
+{
+    Console.WriteLine("Good Luck");
+}
