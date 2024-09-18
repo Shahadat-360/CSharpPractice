a7813dda7683c8b94e6fdfc30ba0df9f506a0c5d
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-18 20:16:32 +0600

    Thread Working

---
+++ Threading/Program.cs
@@ -0,0 +1,17 @@
+﻿void PrintOddNumbers()
+{
+    for (int i = 1;i<=100;i+=2)
+        Console.WriteLine(i);
+}
+
+void PrintEvenNumbers()
+{
+    for (int i = 0; i <= 100; i+=2)
+        Console.WriteLine(i);
+}
+
+Thread t1 = new Thread(()=>PrintOddNumbers());
+Thread t2 = new Thread(()=>PrintEvenNumbers());
+
+t1.Start();
+t2.Start();
