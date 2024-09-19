62eacb1b2bbf51c839ec206bf2b222c625409ad7
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-20 00:17:32 +0600

    Indexer Creation

---
+++ Indexer/Program.cs
@@ -0,0 +1,5 @@
+﻿using Indexer;
+
+ShoppingCart cart = new ShoppingCart();
+cart[0] = new Product();
+cart[1] = new Product();
---
+++ Indexer/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    public class ShoppingCart
+    {
+        private List<Product> products = new List<Product>();
+        public Product this[int Position]
+        {
+            get
+            {
+                return products[Position];
+            }
+            set
+            {
+                products.Add(value);
+                if(Position<products.Count)
+                    products[Position] = value;
+            }
+        }
+    }
+}
