79fe962ece2aa06ed5025a42c3c38af2834507c0
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-20 00:51:54 +0600

    Records Operation

---
+++ Records/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Records
+{
+    public record Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }    
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void Walking()
+        {
+            Console.WriteLine("Person Walking");
+        }
+    }
+}
---
+++ Records/Program.cs
@@ -0,0 +1,20 @@
+﻿using Records;
+
+Person person=new Person("Shahadat",25);
+Person person2=new Person("Sakib",28);
+Person person3 = new Person("Shahadat", 25);
+
+person2 = person;
+person2.Name = "Sakib";
+
+Console.WriteLine(person.Name);
+Console.WriteLine(person.Age);
+
+person3.Name = "Sakib";
+person3.Age = 25;
+
+//Major difference reference not same still output showing same 
+if (person == person3)
+    Console.WriteLine("Same");
+else
+    Console.WriteLine("Not Same");
