847c42b7380040f883aa2e2e5e4d0da57779ff74
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-21 02:29:42 +0600

    Reflection

---
+++ ReflectionExamples/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+
+Type t = typeof(Math);
+MethodInfo[] methods = t.GetMethods();
+int index = 1;
+foreach (MethodInfo method in methods)
+{
+    Console.Write($"{index++}) ");
+    ParameterInfo[] parameters = method.GetParameters();
+    Type returnType = method.ReturnType;
+    Console.Write(returnType.Name);
+    Console.Write(' ');
+    Console.Write(method.Name);
+    Console.Write("(");
+    if (parameters.Length > 0)
+    {
+        foreach (ParameterInfo param in parameters)
+        {
+            Console.Write(param.ParameterType.Name);
+            Console.Write(" ");
+            Console.Write(param.Name);
+            if (parameters[parameters.Length - 1].Name != param.Name)
+            {
+                Console.Write(',');
+            }
+        }
+    }
+    Console.Write(");");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Please Select a Method");
+int selection = int.Parse(Console.ReadLine());
+var selectedMethod = methods[selection - 1];
+var selectedMethodParameters = selectedMethod.GetParameters();
+object[] selectedMethodParamsValue = new object[selectedMethodParameters.Length];
+
+for (int i = 0; i < selectedMethodParameters.Length; i++)
+{
+    var ParameterType = selectedMethodParameters[i].ParameterType;
+    var ParameterMethodType = ParameterType.GetMethod("Parse", new Type[] { typeof(string) });
+
+    var paramValue = Console.ReadLine();
+    selectedMethodParamsValue[i] = ParameterMethodType.Invoke(null, new object[] { paramValue });
+}
+
+var result = selectedMethod.Invoke(null, selectedMethodParamsValue);
+Console.WriteLine(result);
