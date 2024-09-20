ad8d30b8e202769de31ebd1a40799a80a5680a9c
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-21 00:44:54 +0600

    New Switch Case

---
+++ NewSwitchCase/Program.cs
@@ -0,0 +1,22 @@
+﻿using NewSwitchCase;
+
+var x = 0;
+//var option = 2;
+//x = option switch
+//{
+//    0 => x + 5,
+//    1 => 1,
+//    2 => 2,
+//    _ => 12
+//};
+
+var person = new Person { Name = "Shahadat", Age = 25 };
+
+x = person switch
+{
+    { Name:"jalal"} => x+1,
+    { Age:26}=>25,
+    { Name:_,Age:_}=> 0
+};
+
+Console.WriteLine(x);
