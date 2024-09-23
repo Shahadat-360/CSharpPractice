b7b170cd392a4523570f2e51a498c60ebf490045
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-23 23:47:38 +0600

    helper

--- LeetCodeHelper/Program.cs
+++ LeetCodeHelper/Program.cs
@@ -1,14 +1,35 @@
-﻿int n; 
-int[][] roads = new int[2][];
-Dictionary<int, int> map = new Dictionary<int, int>();
-for (int i = 0; i < roads.Length; i++)
+﻿public class Solution
 {
-    int cell = roads[i][0];
-    map[cell] = map.GetValueOrDefault(cell, 0) + 1;
+    private List<int> Solver(string s)
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '+' || s[i] == '-' || s[i] == '*')
+            {
+                var left = Solver(s.Substring(0, i));
+                var right = Solver(s.Substring(i + 1));
+
+                foreach (var x in left)
+                {
+                    foreach (var y in right)
+                    {
+                        if (s[i] == '+') result.Add(y + x);
+                        else if (s[i] == '-') result.Add(x - y);
+                        else result.Add(x * y);
+                    }
+                }
+            }
+        }
+        if (result.Count == 0)
+        {
+            result.Add(int.Parse(s));
+            return result;
+        }
+        return result;
+    }
+    public IList<int> DiffWaysToCompute(string s)
+    {
+        return Solver(s); 
+    }
 }
-for (int i = 0; i < roads.Length; i++)
-{
-    int cell = roads[i][1];
-    map[cell] = map.GetValueOrDefault(cell, 0) + 1;
-}
-var m = map.OrderByDescending
